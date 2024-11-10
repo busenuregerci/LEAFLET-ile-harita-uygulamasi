c4c834477a0adcc576b3eb69f7121134e0e96c64
Buse Nur Eğerci <117750697+busenuregerci@users.noreply.github.com>
2024-11-10 18:13:44 +0300

    harita oluşturuldu ve bir kaç özellik denendi

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,34 @@
-var map = L.map('map').setView([51.505, -0.09], 13); //! map html tarafındaki map olmak zorunda.
+var map = L.map('map').setView([41.00827163916992, 28.966947820848873], 15); //! map html tarafındaki map olmak zorunda.
                                                     //* setView([enlem, boylam], zoomSeviyesi)
 
-var baseMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map); //* baseMap haritaya (map'e) eklendi
+var googleBaseMap = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    attribution : "Proje 1",
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+});
 
-//* marker oluşturuldu
-var marker = L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+googleBaseMap.addTo(map);
 
+
+// var baseMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map); //* baseMap haritaya (map'e) eklendi
+
+// //* marker oluşturuldu
+// var marker = L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//     .openPopup();
+
+
+var nokta1 = L.marker([38,32]).addTo(map);
+
+var cordList1 = [[38,32],[38,33],[39,27]];
+var çizgi1 = L.polyline(cordList1).addTo(map);
+
+
+cordList1.push([39,34]);
+var polygon1 = L.polygon(cordList1).addTo(map);
+
+var bbox = polygon1.getBounds(); //* haritaya ekleninen poligonun bounce değeri alınır ve haritaya fitBounce denildiğinde bizim çerçevelediğimiz çerçeve içerisine haritayı ortalamaya çalışır ve odaklar.
+map.fitBounds(bbox);
     
